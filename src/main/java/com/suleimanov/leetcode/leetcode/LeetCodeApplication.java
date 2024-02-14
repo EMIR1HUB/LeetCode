8f960ee292d0306d8d459c149fde61091d6820df
EMIR1HUB <emir-23.03@mail.ru>
2024-02-14 15:21:57 +0300

    day03 Two Sum: https://leetcode.com/problems/two-sum/description/?source=submission-noac

--- src/main/java/com/suleimanov/leetcode/leetcode/LeetCodeApplication.java
+++ src/main/java/com/suleimanov/leetcode/leetcode/LeetCodeApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class LeetCodeApplication implements CommandLineRunner {
 
@@ -12,12 +14,15 @@ public class LeetCodeApplication implements CommandLineRunner {
     SpringApplication.run(LeetCodeApplication.class, args);
   }
 
-  @Autowired Solutions solutions;
+  @Autowired
+  Solutions solutions;
 
   @Override
-  public void run(String... args) throws Exception{
-    String[] words = {"abc", "po", "car","ada","racecar","cool"};
-    System.out.println(solutions.firstPalindrome(words));
+  public void run(String... args) throws Exception {
+    System.out.println(Arrays.toString(solutions.twoSum(new int[]{3,2,4}, 6)));
+
+//    String[] words = {"abc", "po", "car","ada","racecar","cool"};
+//    System.out.println(solutions.firstPalindrome(words));
 
 //    int[] nums = {3, 2, 3};
 //    System.out.println(solutions.majorityElement(nums));
--- src/main/java/com/suleimanov/leetcode/leetcode/Solutions.java
+++ src/main/java/com/suleimanov/leetcode/leetcode/Solutions.java
@@ -2,8 +2,27 @@ package com.suleimanov.leetcode.leetcode;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class Solutions {
+
+  // day03 Two Sum  https://leetcode.com/problems/two-sum/description/?source=submission-noac
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int current = nums[i];
+      // current + x = target
+      // x = target - current
+      int x = target - current;
+      if (map.containsKey(x)) {
+        return new int[]{i, map.get(x)};
+      }
+      map.put(current, i);
+    }
+    return null;
+  }
 
   // day02 Find First Palindromic String in the Array https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/
   public String firstPalindrome(String[] words) {
